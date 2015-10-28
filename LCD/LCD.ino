3d5420d9c5fb849a3f5934cb5c38014465d09be1
mquig42 <mquig42@users.noreply.github.com>
2015-10-28 11:12:00 -0400

    Update LCD.ino
    Refactor in preparation for adding select and set buttons

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -14,8 +14,18 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 char select = 0;
 char set = 0;
-volatile uint32_t seconds = 0;
-volatile char ledState = 0;
+volatile uint32_t global_seconds = 0;
+
+//Timer interrupt is fired once per second.
+//Increment seconds counter, toggle pin 13 LED, reset to 0 at midnight.
+ISR(TIMER1_COMPA_vect)
+{
+    global_seconds++;
+    digitalWrite(13, !digitalRead(13));
+    //Reset to 0 after 24 hours
+    if(global_seconds >= 86400)
+        global_seconds = 0;
+}
 
 void setup()
 {
@@ -23,9 +33,12 @@ void setup()
     pinMode(9, INPUT_PULLUP);
     pinMode(10, INPUT_PULLUP);
     pinMode(13, OUTPUT);
+    configureTimer();
+}
 
-    //Set up timer1 to count seconds
-    //This is specific to the Atmel 328
+//Configure timer1 to fire an interrupt once every second
+void configureTimer()
+{
     noInterrupts();
     TCNT1 = 0;
     TCCR1A = 0;
@@ -37,22 +50,18 @@ void setup()
     interrupts();
 }
 
-ISR(TIMER1_COMPA_vect)
+void loop()
 {
-    seconds++;
-    ledState = !ledState;
-    digitalWrite(13, ledState);
-    //Reset to 0 after 24 hours
-    if(seconds >= 86400)
-        seconds = 0;
+    dispTime();
 }
 
-void loop()
+//Display time on the first line of the LCD in HH:MM:SS format
+void dispTime()
 {
     uint32_t local_seconds;
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
-        local_seconds = seconds;
+        local_seconds = global_seconds;
     }
     uint8_t hours = local_seconds / 3600;
     uint8_t minutes = (local_seconds % 3600) / 60;
@@ -71,9 +80,15 @@ void loop()
     if(secs<10)
         lcd.print("0");
     lcd.print(secs);
+}
 
-    //Print millis on bottom line of lcd
-    lcd.setCursor(0, 1);
-    lcd.print(millis() / 1000);
+//Resets the seconds portion of the current time to 0, while preserving hours and minutes.
+void resetSeconds()
+{
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        uint8_t secs = global_seconds % 60;
+        global_seconds -= secs;
+    }
 }
 
